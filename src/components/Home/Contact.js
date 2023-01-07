896ab640706c5ca6e8e37bb672b64319cb28692a
Koushikpaul7 <koushikpaul6211@gmail.com>
2023-01-07 23:55:52 +0600

    zoom effect done

--- src/components/Home/Contact.js
+++ src/components/Home/Contact.js
@@ -5,9 +5,11 @@ const Contact = () => {
         <div className='container mt-5'>
             <h3 className='text-center mt-5 mb-4 bg-danger py-2 text-white'>Contact us</h3>
             <div className='row g-5'>
-            <div className='col-12 col-md-6'>
-                    <img src="https://media.istockphoto.com/id/1365543480/video/contact-us-concept.jpg?s=640x640&k=20&c=G16GzQ4uJKlITww2ArmKNB9UKb1-IT2eyJrUxkUmAIY=" alt="" className='img-fluid rounded h-full zoom'/>
+            <div className='col-12 col-md-6 zoom'>
+                  <div className='imgContainer'>
+                  <img src="https://media.istockphoto.com/id/1365543480/video/contact-us-concept.jpg?s=640x640&k=20&c=G16GzQ4uJKlITww2ArmKNB9UKb1-IT2eyJrUxkUmAIY=" alt="" className='img-fluid rounded h-full zoom'/>
                     
+                  </div>
                 </div>
                 <div className='col-12 col-md-6'>
                     <form>
--- src/components/Home/Economics.js
+++ src/components/Home/Economics.js
@@ -6,47 +6,85 @@ const Economics = () => {
             <h2 className='text-info'>Economics</h2>
             <div className='row'>
                 {/* first part */}
-  
-                <div className='col-12 col-md-3 intro-image-box'>
-                    <img src="https://cdn.jagonews24.com/media/imgAllNew/SM/2019November/samson-h-chowdhury-20230107142137.jpg" className='img-fluid  ' alt="" />
+
+                <div className='col-12 col-md-3  '>
+                    <div className='zoom'>
+
+                        <div className='imgContainer'>
+                            <img src="https://cdn.jagonews24.com/media/imgAllNew/SM/2019November/samson-h-chowdhury-20230107142137.jpg" className='img-fluid  ' alt="" />
+                        </div>
+                    </div>
                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
                     <div className='row gap-2'>
-                        <div className='col-12 d-flex my-2'>
-                            <img src="https://cdn.jagonews24.com/media/imgAllNew/SM/2019November/warm1-20230107152001.jpg" className='img-fluid w-50 h-50 me-3 mt-2' alt=""/>
-                            <p className='fs-6'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+                        <div className='col-12 d-flex my-2 zoom'>
+                            <div className='imgContainer'>
+                                <img src="https://cdn.jagonews24.com/media/imgAllNew/SM/2019November/warm1-20230107152001.jpg" className='img-fluid mt-2' alt="" />
+
+                            </div>
+                            <div className='ms-3'>
+
+                                <p className='fs-6'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+                            </div>
                         </div>
-                        <div className='col-12 d-flex'>
-                            <img src="https://cdn.jagonews24.com/media/imgAllNew/SM/2019November/warm1-20230107152001.jpg" className='w-50 h-50 me-3 mt-2' alt=""/>
-                            <p className='fs-6'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. ...</p>
+                        <div className='col-12 d-flex zoom'>
+                            <div className='imgContainer'>
+
+                                <img src="https://cdn.jagonews24.com/media/imgAllNew/SM/2019November/warm1-20230107152001.jpg" className='img-fluid  mt-2' alt="" />
+                            </div>
+                            <div className='ms-3'>
+
+                                <p className='fs-6 '>Lorem Ipsum is simply dummy text of the printing and typesetting industry. ...</p>
+                            </div>
                         </div>
                     </div>
                 </div>
 
                 {/* middle part */}
-                <div className='col-12 col-md-6'>
-                <img src="https://cdn.jagonews24.com/media/imgAllNew/BG/2019November/oil-cv-20230107153121.jpg" className='img-fluid zoom ' alt="" />
-                <h1>Lorem Ipsum is simply dummy text of the printing.</h1>
+                <div className='col-12 col-md-6 zoom'>
+                    <div className='imgContainer'>
+
+                        <img src="https://cdn.jagonews24.com/media/imgAllNew/BG/2019November/oil-cv-20230107153121.jpg" className='img-fluid ' alt="" />
+                    </div>
+                    <h1>Lorem Ipsum is simply dummy text of the printing.</h1>
                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                    Lorem Ipsum has been the industry's standard dummy text ever since the
-                    1500s, when an unknown printer took a galley of type and scrambled it to
-                    make a type specimen book. </p>
+                        Lorem Ipsum has been the industry's standard dummy text ever since the
+                        1500s, when an unknown printer took a galley of type and scrambled it to
+                        make a type specimen book. </p>
                 </div>
 
-                    {/* Last part */}
+                {/* Last part */}
                 <div className='col-12 col-md-3'>
-                <img src=" https://cdn.jagonews24.com/media/imgAllNew/SM/2019November/dse-20230107115314.jpg" className='img-fluid zoom ' alt="" />
+                    <div className='zoom'>
+                        <div className='imgContainer'>
+
+                            <img src=" https://cdn.jagonews24.com/media/imgAllNew/SM/2019November/dse-20230107115314.jpg" className='img-fluid  ' alt="" />
+                        </div>
+                    </div>
                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
                     <div className='row gap-2'>
-                        <div className='col-12 d-flex my-2'>
-                            <img src="https://cdn.jagonews24.com/media/imgAllNew/SM/2019November/warm1-20230107152001.jpg" className='img-fluid w-50 h-50 me-3 mt-2' alt=""/>
-                            <p className='fs-6 '>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+                        <div className='col-12 d-flex my-2 zoom'>
+                            <div className='imgContainer'>
+                                <img src="https://cdn.jagonews24.com/media/imgAllNew/SM/2019November/warm1-20230107152001.jpg" className='img-fluid mt-2' alt="" />
+
+                            </div>
+                            <div className='ms-3'>
+
+                                <p className='fs-6'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+                            </div>
                         </div>
-                        <div className='col-12 d-flex my-2'>
-                            <img src="https://cdn.jagonews24.com/media/imgAllNew/SM/2019November/warm1-20230107152001.jpg" className='img-fluid w-50 h-50 me-3 mt-2' alt=""/>
-                            <p className='fs-6 '>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+                        <div className='col-12 d-flex my-2 zoom'>
+                            <div className='imgContainer'>
+                                <img src="https://cdn.jagonews24.com/media/imgAllNew/SM/2019November/warm1-20230107152001.jpg" className='img-fluid mt-2' alt="" />
+
+                            </div>
+                            <div className='ms-3'>
+
+                                <p className='fs-6'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+                            </div>
                         </div>
-                        </div>
-               
+                      
+                    </div>
+
                 </div>
             </div>
         </div>
